7585c0f192b3dc37b9b832e57332de5158527cb7
NoemieBenard <noemie.benard@gmail.com>
2025-02-23 15:50:27 +0100

    display current weather

--- site_bière/script.js
+++ site_bière/script.js
@@ -1,43 +1,52 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.placeholder = 'Enter a task';
-    document.body.appendChild(input);
+const baseUrl = 'http://www.7timer.info/bin/civil.php?'
 
-    const ul = document.createElement('ul');
-    document.body.appendChild(ul);
 
-    input.addEventListener('keypress', function (e) {
-        if (e.key === 'Enter') {
-            const task = input.value.trim();
-            if (task) {
-                const li = document.createElement('li');
-                li.textContent = task;
-                ul.appendChild(li);
-                input.value = '';
-            }
-        }
-    });
-    ul.addEventListener('click', function (e) {
-        if (e.target.tagName === 'LI') {
-            ul.removeChild(e.target);
-        }
-    });
-});
-
-async function fetchBeerByUrl(url){
+async function fetchByCoordinates(lon, lat){
+    const url = baseUrl + 'lon=' + lon + '&lat='+ lat + '&ac=0&lang=en&unit=metric&output=json&tzshift=0'
+    console.log(url);
     const httpRes = await fetch(url);
     const data = await httpRes.json();
     return data;
 }
 
-async function displayBeer(beer) {
-    const cardContainer = document.getElementById("card-container");
-    const beerCard = document.createElement("div");
-    beerCard.classList.add("card", "h-100", "shadow-sm");
+async function displayWeather(weather_data, city_data) {
+    const weatherCard = document.getElementById("weather-card");
 
-    cardContainer.appendChild(beerCard);
-    
+    //city
+    const cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title", "text-center");
+    console.log(city_data[2])
+    cardTitle.textContent = `${city_data[2]}`;
+    weatherCard.appendChild(cardTitle);
+
+    //weather icon
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const cardIcon = document.createElement("img");
+    cardIcon.src = '../image/' + weather_data.dataseries[0].weather + '.png';
+    cardIcon.classList.add("img-fluid", "me-3");
+    cardBody.appendChild(cardIcon);
+
+    weatherCard.appendChild(cardBody);
 }
 
-console.log(fetchBeerByUrl('https://api.punkapi.com/v2/beers/1'));
+async function getCoordinates(city) {
+    const url = 'https://nominatim.openstreetmap.org/search?city=' + city + '&format=json';
+    const httpRes = await fetch(url);
+    const data = await httpRes.json();
+    const lon = data[0].lon;
+    const lat = data[0].lat;
+    const name = data[0].display_name;
+    return [lon, lat, name];
+}
+
+async function main() {
+    const info = await getCoordinates('nancy');
+    const data = await fetchByCoordinates(info[0], info[1]);
+    console.log(data);
+    displayWeather(data, info);
+}
+
+main();
+
